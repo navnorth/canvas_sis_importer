7b0d06a3377d859fdcc40d030ffb5767fbb5caca
joe hobson <joe@navigationnorth.com>
2019-09-06 12:37:44 -0500

    exclude some Admins and Teachers based on Title

--- sql_scripts/select_admins_testing_enrollment.sql
+++ sql_scripts/select_admins_testing_enrollment.sql
@@ -15,42 +15,45 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+DROP TABLE IF EXISTS excludeTitlesList;
+CREATE TEMP TABLE excludeTitlesList (exclude_title) AS VALUES ('ATHLETICS DIRECTOR','AVID TUTOR','BOOKKEEPER: HIGH SCHOOL','ED ASST: HEALTH','EDUC ASST: BEHAVIOR REDIRECTOR','EDUC ASST: CHAPTER 1','EDUC ASST: CHILD DEVELOPMENT','EDUC ASST: HEALTH','EDUC ASST: KINDER PRE','EDUC ASST: TITLE I','EDUCATIONAL ASST: HEALTH: ES','INTERPRETER FOR THE DEAF','LIAISON: EARLY CHILDHOOD','MANAGER: TITLE I INSTRUCTIONAL','NURSE','PATHOLOGIST: RELATED SERVICES','PATHOLOGIST: SPEECH-LANGUAGE','RES TEACHER: SP ED/PROF DEV','RESOURCE TEACHER: MUSIC','SOCIAL WORKER','SOCIAL WORKER-SP ED','SPECIALIST: BEHAVIOR MGMT','TEACHER: ADAPTED PE','TEACHER: BEHAVIOR INTERVENTION','TEACHER: CHILDFIND','TEACHER: ITINERANT ART','TEACHER: ITINERANT MUSIC','TEACHER: MUSIC/CHOR/BND/ORCH','TEACHER: TITLE I','TEACHER: TITLE I READING RECOV','TEACHER: TITLE VII','TEACHER: TRANSITION EARLY CHIL','TECHNICIAN: CHILDCARE','TEMP/HOURLY: ATHLETICS COACH','TEMP/HOURLY: ATHLETICS COACH','TEMPS/HOURLY: NON APS EMP','TEMPS/HOURLY: OTHER','TEMPS/HOURLY: TEACHER','THERAPIST: OCCUPATIONAL','THERAPIST: PHYSICAL','TRANS SPEC-REHAB COUNSELOR');
+
 SELECT DISTINCT
     'TEST_SEC_STC' as course_id,
-    trim(staff_id) as user_id,
+    trim(A.staff_id) as user_id,
     'student' as role,
     '' as role_id,
     '' as section_id,
     'active' as status
 from
-    sis_import_admins
-WHERE (trim(schedule) = 'SPE')
+    sis_import_admins A LEFT OUTER JOIN excludeTitlesList E ON trim(A.admin_title) = E.exclude_title
+WHERE (trim(A.schedule) = 'SPE')
 
 UNION
 
 SELECT DISTINCT
     'TEST_SEC_TA' as course_id,
-    trim(staff_id) as user_id,
+    trim(A.staff_id) as user_id,
     'student' as role,
     '' as role_id,
     '' as section_id,
     'active' as status
 from
-    sis_import_admins
-WHERE (trim(schedule) LIKE 'A SCHED%')
+    sis_import_admins A LEFT OUTER JOIN excludeTitlesList E ON trim(A.admin_title) = E.exclude_title
+WHERE (trim(A.schedule) LIKE 'A SCHED%')
 
 UNION
 
 SELECT DISTINCT
     'TEST_SEC_STAFF' as course_id,
-    trim(staff_id) as user_id,
+    trim(A.staff_id) as user_id,
     'student' as role,
     '' as role_id,
     '' as section_id,
     'active' as status
 from
-    sis_import_admins
-WHERE (trim(schedule) = 'G1 SCHED')
+    sis_import_admins A LEFT OUTER JOIN excludeTitlesList E ON trim(A.admin_title) = E.exclude_title
+WHERE (trim(A.schedule) = 'G1 SCHED')
 
 
 /* put all teachers in TA course */
@@ -58,15 +61,15 @@ UNION
 
 SELECT DISTINCT
     'TEST_SEC_TA' as course_id,
-    trim(teacher_id) as user_id,
+    trim(T.teacher_id) as user_id,
     'student' as role,
     '' as role_id,
     '' as section_id,
     'active' as status
 from
-    sis_import_teachers
+    sis_import_teachers T LEFT OUTER JOIN excludeTitlesList E ON trim(T.title) = E.exclude_title
 WHERE
-    teacher_id not in (
+    T.teacher_id not in (
         'e999008','e999101','e999102','e999103','e999105','e999998','e999999',
         '777777777',
         'closed_sec',
