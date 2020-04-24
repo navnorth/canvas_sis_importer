4c438fbd498756d06e4d0c31c2220eca459d4675
joe hobson <joe@navigationnorth.com>
2020-04-24 14:17:34 -0500

    update exclude admins query

--- sql_scripts/excluded_admins.sql
+++ sql_scripts/excluded_admins.sql
@@ -3,10 +3,9 @@ SELECT DISTINCT
     trim(D.staff_id) as user_id,
     '' as integration_id,
     trim(D.staff_id) as login_id,
-    '3' as authentication_provider_id,
     INITCAP(trim(D.first_name)||' '||trim(D.last_name)) as full_name,
     trim(D.admin_email) as email,
-    'active' as status, D.Schedule,
+    D.Schedule,
     D.school_id, S.School_name
 FROM sis_import_admins D
     LEFT JOIN sis_import_schools S ON S.school_id = D.school_id
